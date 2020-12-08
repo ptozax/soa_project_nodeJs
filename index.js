ec9e4f91b3f97371701b2097d7ea346a104ba276
ptozax <38245998+ptozax@users.noreply.github.com>
2020-12-08 20:02:16 +0700

    1

---
+++ index.js
@@ -0,0 +1,87 @@
+
+
+
+const express = require("express")
+const bodyParser = require("body-parser")
+
+
+
+//**************************************************** */
+
+// const csvtojson=require('csvtojson');
+// const csvfilepath="csv/covid19-confirmed.csv"
+// csvtojson()
+// .fromFile(csvfilepath)
+// .then((json)=>{
+
+// console.log(json)
+
+
+// })
+
+
+//***************************************************** */
+
+
+
+
+
+
+const router = express.Router()
+router.get("/confirmed", (req, res, next) => {
+const csvtojson = require('csvtojson');
+    const csvfilepath = "csv/covid19-confirmed.csv"
+    csvtojson()
+        .fromFile(csvfilepath)
+        .then((json) => {
+
+
+            return res.send(json)
+
+        })
+})
+
+router.get("/death", (req, res, next) => {
+    const csvtojson = require('csvtojson');
+        const csvfilepath = "csv/covid19-death.csv"
+        csvtojson()
+            .fromFile(csvfilepath)
+            .then((json) => {
+    
+    
+                return res.send(json)
+    
+            })
+    })
+    router.get("/recovered", (req, res, next) => {
+        const csvtojson = require('csvtojson');
+            const csvfilepath = "csv/covid19-recovered.csv"
+            csvtojson()
+                .fromFile(csvfilepath)
+                .then((json) => {
+        
+        
+                    return res.send(json)
+        
+                })
+        })
+
+
+
+
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+
+}))
+
+
+
+app.use(router)
+app.listen(3000, () => {
+
+    console.log('Server is running on port', 3000)
+
+})
